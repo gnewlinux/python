5196e44b07f165fd30834893cf0db3470f4a5481
gnewlinux <gnewlinux@gmail.com>
2017-08-07 00:18:38 -0300

    news

--- tubedown/tubedown.py
+++ tubedown/tubedown.py
@@ -3,55 +3,64 @@ import os
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import filedialog
+import webbrowser
 
-def abrir_video():
+
+# FUNCTIONS
+
+def clickAbout():
+    about = Toplevel()
+    about.title('About')
+    about.geometry('200x100+500+200')
+    label1 = Label(about, text='gnew.2017', justify='center')
+    label1.pack(padx=10, pady=(25, 10))
+    label2 = Label(about, text='good downloads!', justify='center')
+    label2.pack(padx=10)
+
+def logo_link():
+	webbrowser.open("https://youtube.com")
+
+def open_video():
 	os.system('vlc %s' % filename)
 	toplevel.destroy()
 
-def logo():
-	webbrowser.open("https://youtube.com")
-
-def play():
-	#url = 'https://www.youtube.com/watch?v=XjI889CH9qw&t=1s'
+def download_func():
 	url = video.get()
 	download.set('Wait...')
 
 	if url != '' and validaUrl(url):
 		global filename
 		filename = filedialog.asksaveasfilename(filetypes=[("MP4 Files", "*.mp4")])
-		termf = Frame(janela, height=19, width=84)
-		termf.pack(fill=BOTH, expand=YES)
-		wid = termf.winfo_id()
-		os.system('xterm -geometry 95x10+380+510 -sb -e youtube-dl -f mp4 -o %s %s' % (filename, url))
-		#text = Text(toplevel, height=19, width=84)
-		#text.pack() 
-		#sistema = os.system('youtube-dl -f mp4' + url)
 
-		#p = sub.Popen(['youtube-dl -f mp4 -o ' + filename + ' ' + url], stdout=sub.PIPE,stderr=sub.PIPE, shell=True)
+		os.system('xterm -geometry 95x10+380+510 -sb -e youtube-dl -f mp4 -o %s %s' % (filename, url))
+
+		## STILL NOT RUNNING :(
 		progress['value']=20
-		janela.update_idletasks()
+		root.update_idletasks()
 		progress['value']=50
-		janela.update_idletasks()
+		root.update_idletasks()
 		progress['value']=80
-		janela.update_idletasks()
+		root.update_idletasks()
 		progress['value']=100
 
-		global toplevel
+		## NEW WINDOW
 		toplevel = Toplevel()
 		toplevel.geometry('200x100')
+
 		label_top = Label(toplevel, text='Download finished.')
 		label_top.pack(pady=20)
+
 		btn_top = Button(toplevel, text='OK', command=toplevel.destroy)
 		btn_top.pack(side=LEFT, padx=8)
-		btn_ver_top = Button(toplevel, text='OPEN', command=abrir_video)
-		btn_ver_top.pack(side=RIGHT, padx=8)
-		
 
-		#output = p.communicate()
-		#text.insert(END, output)
+		btn_ver_top = Button(toplevel, text='OPEN', command=open_video)
+		btn_ver_top.pack(side=RIGHT, padx=8)
+
+		video.set('')
+		progress['value']=0
 
 	else:
-		video.set('Url invalida')
+		video.set('INVALID URL')
 
 	download.set('Download')
 
@@ -59,71 +68,61 @@ def validaUrl(url):
 	ref = 'https://www.youtube.com'
 	return url.startswith(ref)
 
-def limpar(event):
+def clear(event):
 	video.set('')
 	progress['value']=0
 
-def sair():
-	janela.destroy()
+def exit():
+	root.destroy()
 
-def clickAbout():
-    toplevel = Toplevel()
-    toplevel.title('About')
-    toplevel.geometry('200x100+500+200')
-    label1 = Label(toplevel, text='gnew 2017', width=30)
-    label1.pack()
-    label2 = Label(toplevel, text='github.com/gnewlinux/', width=30)
-    label2.pack()
 
-janela = Tk()
-janela.title('tube_down')
-janela.geometry('220x200')
-#janela.iconbitmap('tubedown.ico')
-#img = PhotoImage(file='img/tubedown.ico')
-#janela.call('wm', 'iconphoto', janela._w, img)
+## TK MAIN LOOP
+root = Tk()
+root.title('tube_down')
+root.geometry('220x200')
 
 video = StringVar()
 
-# MENU BAR
-menubar = Menu(janela)
+## MENU BAR
+menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
-#filemenu.add_command(label="Save as", command=file_save)
-#filemenu.add_separator()
-filemenu.add_command(label="Exit", command=janela.quit)
+
+filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 
 helpmenu = Menu(menubar, tearoff=0)
 helpmenu.add_command(label="About", command=clickAbout)
 menubar.add_cascade(label="Help", menu=helpmenu)
 
-janela.config(menu=menubar, borderwidth=0, highlightthickness=0)
+root.config(menu=menubar, borderwidth=0, highlightthickness=0)
 
 
 ## WIDGETS
-foto = PhotoImage(file='img/youtube.png')
-lb_logo = Label(janela)
-lb_logo['image'] = foto
+logo = PhotoImage(file='img/youtube.png')
+lb_logo = Label(root)
+lb_logo['image'] = logo
+url = 'https://youtube.com'
+lb_logo.bind("<Button-1>",lambda e,url=url:logo_link())
 lb_logo.pack()
 
-lb_txt = Label(janela, text='Enter URL Youtube: ')
+lb_txt = Label(root, text='Enter URL Youtube: ')
 lb_txt.pack()
 
-ed = Entry(janela, textvariable=video)
+ed = Entry(root, textvariable=video)
 ed.pack()
+ed.bind('<Button-1>', clear)
 
-progress=Progressbar(janela,orient=HORIZONTAL,length=500,mode='determinate')
+progress=Progressbar(root,orient=HORIZONTAL,length=500,mode='determinate')
 progress.pack(side=BOTTOM)
 
 download = StringVar()
 download.set('Download')
-btn1 = Button(janela, textvariable=download, command=play)
+btn1 = Button(root, textvariable=download, command=download_func)
 btn1.pack(side=LEFT, padx=20)
 
-btn2 = Button(janela, text='Quit', command=sair)
+btn2 = Button(root, text='Quit', command=exit)
 btn2.pack(side=RIGHT, padx=20)
-
-ed.bind('<Button-1>', limpar)
 
 
 ### LOOP
-janela.mainloop()
+root.mainloop()
