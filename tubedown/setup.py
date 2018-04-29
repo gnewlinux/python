95c4738c42ef98087c976ccd69bbfa06b3a251c8
diegosarzi <diegosarzi@gmail.com>
2018-04-29 13:53:55 -0300

    shutdown

--- tubedown/setup.py
+++ tubedown/setup.py
@@ -1,16 +1,16 @@
 port os, sys, glob
 from distutils.core import setup
 
-libdir = 'share/tubedown'
+libdir = 'share/tubedown/'
 sys.path += [os.path.join(os.curdir, libdir)]
 
 setup(name='tubedown',
       version='0.1',
       description='youtube-dl gui',
       license='GPL3',
-      url='',
+      url='diego.sarzi.com.br',
       author='Diego Sarzi',
       author_email='diegosarzi@gmail.com',
       scripts=['tubedown'],
-      data_files=[(libdir, ['img/youtube.png'])]
+      data_files=[(libdir + ['img/youtube.png'])]
       )
